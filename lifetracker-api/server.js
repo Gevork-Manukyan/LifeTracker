2c17e2aa74918ed23c4bc6dac422873f76284c0d
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-24 13:03:30 -0700

    sets up basic errors in server.js

--- lifetracker-api/server.js
+++ lifetracker-api/server.js
@@ -2,6 +2,9 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 
+const { NotFoundError } = require("./utils/errors")
+
+
 
 const app = express()
 
@@ -14,6 +17,21 @@ app.use(morgan("tiny"))
 
 app.use("/", async (req, res, next) => {
     res.status(200).json({ ping: "pong" })
+})
+
+/** Handle 404 errors -- this matches everything that isn't defined */
+app.use((req, res, next) => {
+    return next(new NotFoundError())
+})
+
+/** Generic error handler; anything unhandled ends up here. */
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    const message = err.message
+
+    return res.status(status).json({
+        error: { message, status }
+    })
 })
 
 PORT = 3001
