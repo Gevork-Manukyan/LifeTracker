b585835d9555330118887add909e427f60decc9b
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-24 10:44:43 -0700

    sets up basic sql schema

---
+++ lifetracker-api/lifetracker-schema.sql
@@ -0,0 +1,46 @@
+CREATE TABLE users (
+    id          SERIAL PRIMARY KEY,
+    password    TEXT NOT NULL,
+    email       TEXT NOT NULL,
+    is_admin    BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE activity_logs (
+    id          SERIAL PRIMARY KEY,
+    user_id     INTEGER NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE activity_details (
+    activity_log_id INTEGER NOT NULL,
+    item_category   TEXT NOT NULL,
+    item_id         INTEGER NOT NULL
+    PRIMARY KEY (activity_log_id, item_category, item_id),
+    FOREIGN KEY (activity_log_id) REFERENCES activity_logs(id) ON DELETE CASCADE
+    FOREIGN KEY (item_id) REFERENCES (SELECT )
+);
+
+CREATE TABLE exercise_items (
+    id          SERIAL PRIMARY KEY,
+    name        TEXT NOT NULL,
+    duration    INTEGER NOT NULL,
+    intensity   INTEGER NOT NULL DEFAULT 1 CHECK (intensity BETWEEN 1 AND 10),
+
+);
+
+CREATE TABLE sleep_items (
+    id          SERIAL PRIMARY KEY,
+    date        DATE NOT NULL,      --'yyyy-mm-dd' 
+    start_time  TIME (0) NOT NULL,  --'hh:mm:ss' hh: 0-23  mm: 0-59  ss: 0-59
+    end_time    TIME (0) NOT NULL
+);
+
+CREATE TABLE nutritional_items (
+    id          SERIAL PRIMARY KEY,
+    name        TEXT NOT NULL,
+    calories    INTEGER NOT NULL,
+    quantity    INTEGER NOT NULL DEFAULT 1,
+    category    TEXT
+);
