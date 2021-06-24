d1e5253343c102e9514b78a05089d91e3d3b362a
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-24 10:59:52 -0700

    cleans up schema

--- lifetracker-api/lifetracker-schema.sql
+++ lifetracker-api/lifetracker-schema.sql
@@ -6,41 +6,46 @@ CREATE TABLE users (
     created_at  TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE activity_logs (
-    id          SERIAL PRIMARY KEY,
-    user_id     INTEGER NOT NULL,
-    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+-- CREATE TABLE activity_logs (
+--     id          SERIAL PRIMARY KEY,
+--     user_id     INTEGER NOT NULL,
+--     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+--     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+-- );
+
+-- CREATE TABLE activity_details (
+--     activity_log_id INTEGER NOT NULL,
+--     item_category   TEXT NOT NULL,
+--     item_id         INTEGER NOT NULL
+--     PRIMARY KEY (activity_log_id, item_category, item_id),
+--     FOREIGN KEY (activity_log_id) REFERENCES activity_logs(id) ON DELETE CASCADE
+--     FOREIGN KEY (item_id) REFERENCES 
+-- );
+
+CREATE TABLE exercise_items (
+    id                  SERIAL PRIMARY KEY,
+    user_id             INTEGER NOT NULL,
+    name                TEXT NOT NULL,
+    duration            INTEGER NOT NULL,
+    intensity           INTEGER NOT NULL DEFAULT 1 CHECK (intensity BETWEEN 1 AND 10),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE activity_details (
-    activity_log_id INTEGER NOT NULL,
-    item_category   TEXT NOT NULL,
-    item_id         INTEGER NOT NULL
-    PRIMARY KEY (activity_log_id, item_category, item_id),
-    FOREIGN KEY (activity_log_id) REFERENCES activity_logs(id) ON DELETE CASCADE
-    FOREIGN KEY (item_id) REFERENCES (SELECT )
-);
-
-CREATE TABLE exercise_items (
-    id          SERIAL PRIMARY KEY,
-    name        TEXT NOT NULL,
-    duration    INTEGER NOT NULL,
-    intensity   INTEGER NOT NULL DEFAULT 1 CHECK (intensity BETWEEN 1 AND 10),
-
-);
-
 CREATE TABLE sleep_items (
-    id          SERIAL PRIMARY KEY,
-    date        DATE NOT NULL,      --'yyyy-mm-dd' 
-    start_time  TIME (0) NOT NULL,  --'hh:mm:ss' hh: 0-23  mm: 0-59  ss: 0-59
-    end_time    TIME (0) NOT NULL
+    id                  SERIAL PRIMARY KEY,
+    user_id             INTEGER NOT NULL,
+    date                DATE NOT NULL,      --'yyyy-mm-dd' 
+    start_time          TIME (0) NOT NULL,  --'hh:mm:ss'   hh: 0-23  mm: 0-59  ss: 0-59
+    end_time            TIME (0) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE nutritional_items (
-    id          SERIAL PRIMARY KEY,
-    name        TEXT NOT NULL,
-    calories    INTEGER NOT NULL,
-    quantity    INTEGER NOT NULL DEFAULT 1,
-    category    TEXT
+    id                  SERIAL PRIMARY KEY,
+    user_id             INTEGER NOT NULL,
+    food_name           TEXT NOT NULL,
+    calories            INTEGER NOT NULL,
+    quantity            INTEGER NOT NULL DEFAULT 1,
+    food_category       TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
