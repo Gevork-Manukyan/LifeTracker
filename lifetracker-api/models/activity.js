d9d55fa975a1a1c9b4f7c6fe16a70a85ecdb1ba3
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-25 10:40:25 -0700

    creates routes & models for list sleeps & nutrits

--- lifetracker-api/models/activity.js
+++ lifetracker-api/models/activity.js
@@ -13,7 +13,37 @@ class Activity {
 
             `, [user.email]
         )
-        
+
+        return results.rows;
+    }
+
+    // list all sleeps related to specific user
+    static async listSleeps (user) {
+        const results = await db.query(
+            `
+                SELECT sItems.id AS "sleepID", name, duration, intensity 
+                FROM users
+                    JOIN sleep_items AS sItems ON users.id = sItems.user_id
+                WHERE user_id = (SELECT id FROM users WHERE email = $1)
+
+            `, [user.email]
+        )
+
+        return results.rows;
+    }
+
+    // list all nutritional items related to specific user
+    static async listNutritions (user) {
+        const results = await db.query(
+            `
+                SELECT nItems.id AS "nutritionalID", name, duration, intensity 
+                FROM users
+                    JOIN nutritional_items AS nItems ON users.id = nItems.user_id
+                WHERE user_id = (SELECT id FROM users WHERE email = $1)
+
+            `, [user.email]
+        )
+
         return results.rows;
     }
 }
--- lifetracker-api/routes/activities.js
+++ lifetracker-api/routes/activities.js
@@ -14,4 +14,24 @@ router.get("/exercises", security.requireAuthenticatedUser, async(req, res, next
     }
 })
 
+router.get("/sleeps", security.requireAuthenticatedUser, async(req, res, next) => {
+    // return all sleep activities associated to user
+    try {
+        const sleeps = Activity.listSleeps(res.locals.user)
+        return res.status(200).json({ sleeps })
+    } catch (err) {
+        return next(err);
+    }
+})
+
+router.get("/nutritions", security.requireAuthenticatedUser, async(req, res, next) => {
+    //return all nutrition items associated to user
+    try {
+        const nutritions = Activity.listNutritions(res.locals.user)
+        return res.status(200).json({ nutritions })
+    } catch (err) {
+        return next(err);
+    }
+})
+
 module.exports = router;
